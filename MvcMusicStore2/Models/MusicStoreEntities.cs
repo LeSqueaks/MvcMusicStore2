41b5caca016c146a143785968dd9b8e08342edcf
Julie Burke <julie.f.burke@gmail.com>
2017-08-22 19:33:08 -0400

    Step 5 Part 1.  Created StoreManager Controller.

--- MvcMusicStore2/Models/MusicStoreEntities.cs
+++ MvcMusicStore2/Models/MusicStoreEntities.cs
@@ -10,5 +10,7 @@ namespace MvcMusicStore2.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<MvcMusicStore2.Models.Artist> Artists { get; set; }
     }
 }
